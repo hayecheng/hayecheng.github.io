683433a1399356a3b7ff1310d6f701b9127bc6a9
Haye <2576391074@qq.com>
2024-11-17 17:34:32 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-output:'export',
-basePath:'/hayecheng.github.io'
   /* config options here */
+  output: "export",
 };
 
 export default nextConfig;
